a12ee9592afd31afa85abb7df50902e4aa504470
Pruthvik <pruthvik@lokveeraj.com>
2025-09-05 21:21:41 +0530

    init

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response} from "express";
+import {client} from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi there")
+});
+
+app.post("/signup", async (req: Request, res: Response)=> {
+  const {username, password} = req.body;
+
+  const user = await client.user.create({
+    data: {
+      username: username,
+      password: password
+    }
+  })
+
+  res.json({
+    message: "Signup successful",
+    id: user.id
+  })
+})
+
+app.listen(3002);
